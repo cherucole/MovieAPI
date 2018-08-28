1b7e678e96afb079e91557fc3cbd6cceab875f95
cherucole <cheruyotcheruyot@gmail.com>
2018-08-28 16:22:36 +0300

    posting reviews working

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .models import review
+from .forms import ReviewForm
+Review = review.Review
 # from .requests import get_movies,get_movie
 
 
@@ -51,16 +54,16 @@ def index():
     myTitle='Home- Welcome to the best movie review site Online'
     return render_template('index.html', title=myTitle)
 
-@app.route('/movie/<int:id>')
-def movie(id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    movie = get_movie(id)
-    title = f'{movie.title}'
-
-    return render_template('movie.html',title = title,movie = movie)
+# @app.route('/movie/<int:id>')
+# def movie(id):
+#
+#     '''
+#     View movie page function that returns the movie details page and its data
+#     '''
+#     movie = get_movie(id)
+#     title = f'{movie.title}'
+#
+#     return render_template('movie.html',title = title,movie = movie)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -72,3 +75,30 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
+
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
