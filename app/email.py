85f8e0aa3a7f1ec19ab2dcd71121e62e8bfcbe18
cherucole <cheruyotcheruyot@gmail.com>
2018-09-04 14:05:06 +0300

    working everything now with email signup message

--- app/email.py
+++ app/email.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-sender_email = 'miladzandiapps@gmail.com'
+sender_email = 'moringamelissa254@gmail.com'
 
 
 
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MOVIE_API_KEY=dc6a9659f2c0f6061ab5ea1b87e6ea5e
 export SECRET_KEY=mysecretkey
-export MAIL_USERNAME=miladzandiapps@gmail.com
-export MAIL_PASSWORD=cincolecheru
+export MAIL_USERNAME=moringamelissa254@gmail.com
+export MAIL_PASSWORD=moringa123
 
 python3.7 manage.py server
